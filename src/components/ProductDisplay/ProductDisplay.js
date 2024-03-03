edbfd20d928a2c91696aecc5276f90189de965ea
Deepu Cyriac <deepucvk123@gmail.com>
2024-03-03 21:07:27 +0530

    Created phase-1 page & component files

---
+++ src/components/ProductDisplay/ProductDisplay.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductDisplay = () => {
+  return (
+    <div>
+      <h3>Product Display</h3>
+    </div>
+  );
+};
+
+export default ProductDisplay;
---
+++ src/components/ProductListing/ProductListing.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductListing = () => {
+  return (
+    <div>
+      <h3>Product Listing</h3>
+    </div>
+  );
+};
+
+export default ProductListing;
