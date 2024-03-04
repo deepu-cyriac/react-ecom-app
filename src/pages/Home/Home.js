bae45092e37363fac9dedaf950fce2a80b2e0f3d
Deepu Cyriac <deepucvk123@gmail.com>
2024-03-04 08:05:26 +0530

    Created sidebar component and updated imports to home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+import Header from "../../components/Header/Header";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Footer from "../../components/Footer/Footer";
+import ProductListing from "../../components/ProductListing/ProductListing";
+
 const Home = () => {
   return (
     <div>
       <h3>Home page</h3>
+      <Header />
+      <Sidebar />
+      <ProductListing />
+      <Footer />
     </div>
   );
 };
