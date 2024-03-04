80b736bb16e6ec6ea4cf4960da18cf8f9a3aa398
Deepu Cyriac <deepucvk123@gmail.com>
2024-03-04 08:13:12 +0530

    Created new files and updated README.md

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
 
+import Navigation from "../Navigation/Navigation";
+import Search from "../Search/Search";
 const Header = () => {
   return (
     <div>
       <h3>Header</h3>
+      <Navigation />
+      <Search />
     </div>
   );
 };
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,12 @@
 import React from "react";
 
+import Filters from "../Filters/Filters";
+
 const Sidebar = () => {
   return (
     <div>
       <h3>Sidebar</h3>
+      <Filters />
     </div>
   );
 };
